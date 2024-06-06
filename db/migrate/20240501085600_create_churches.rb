b63e3dc030835d6194a0fb78515a97e241d23342
Robert O. Alabi <wolo22001@gmail.com>
2024-06-06 11:17:08 +0000

    update the churches files

--- db/migrate/20240501085600_create_churches.rb
+++ db/migrate/20240501085600_create_churches.rb
@@ -2,7 +2,7 @@ class CreateChurches < ActiveRecord::Migration[7.1]
   def change
     create_table :churches do |t|
       t.string :name
-
+      t.string :location
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -44,17 +44,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_233402) do
 
   create_table "churches", force: :cascade do |t|
     t.string "name"
+    t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "conversations", force: :cascade do |t|
-    t.string "title"
-    t.integer "started_by_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "recipient_id"
-    t.integer "sender_id"
   end
 
   create_table "cs", force: :cascade do |t|
@@ -149,17 +141,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_233402) do
     t.bigint "member_id", null: false
   end
 
-  create_table "payments", force: :cascade do |t|
-    t.decimal "amount"
-    t.bigint "member_id", null: false
-    t.string "payment_method"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "payment_category"
-    t.date "date"
-    t.index ["member_id"], name: "index_payments_on_member_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.text "title"
     t.text "description"
@@ -189,5 +170,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_233402) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "members", "users"
-  add_foreign_key "payments", "members"
 end
